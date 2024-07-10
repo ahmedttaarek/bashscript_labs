a42d6b9c946f4946f7028896307f35d1319aedc3
Ahmed Tarek <ahmedttaarek@gmail.com>
2024-07-10 22:08:52 +0300

    Add files via upload
    Lab2 with screenshots of running scripts

---
+++ greet.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Prompt the user for their name
+echo "Please enter your name:"
+read user_name
+
+# Send a greeting
+echo "Hello, $user_name! Welcome!"
---
+++ s2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+x=$1
+echo "Method 1: $x"
+
+source ./s1.sh
+echo "Method 2: $x"
